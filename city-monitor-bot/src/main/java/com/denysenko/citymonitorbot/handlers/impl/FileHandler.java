d3ddd91380996453810f0104bd85eb8882735ece
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2022-12-23 19:41:49 +0200

    23.12.2022 impossible to get file name on upload to put it in DB, so implemented random name generation

--- city-monitor-bot/src/main/java/com/denysenko/citymonitorbot/handlers/impl/FileHandler.java
+++ city-monitor-bot/src/main/java/com/denysenko/citymonitorbot/handlers/impl/FileHandler.java
@@ -5,6 +5,7 @@ import com.denysenko.citymonitorbot.enums.BotStates;
 import com.denysenko.citymonitorbot.handlers.Handler;
 import com.denysenko.citymonitorbot.services.BotUserService;
 import lombok.extern.log4j.Log4j;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.*;
@@ -48,6 +49,7 @@ public class FileHandler implements Handler {
             List<PhotoSize> photos = message.getPhoto();
             PhotoSize photoSize = photos.get(photos.size()-1);
             name = photoSize.getFilePath();
+            if(name == null) name = "someImage_" + RandomStringUtils.randomNumeric(3);
             fileID = photoSize.getFileId();
         } else if(message.hasDocument()){
             Document document = message.getDocument();
